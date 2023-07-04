86a8ad03672afdb86fed5fe21486e3876ecf80b1
KarimjonMurodov <61445376+KarimjonMurodov@users.noreply.github.com>
2023-07-04 22:06:35 +0500

    data type

--- lesson01/lesson01/Program.cs
+++ lesson01/lesson01/Program.cs
@@ -4,9 +4,44 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Karimjon Murodov");
-            Console.WriteLine("29 yosh");
+            
+            byte byteMin = byte.MinValue; 
+            byte byteMax = byte.MaxValue;
+
+            Console.WriteLine("Byte min value: " + byteMin);
+            Console.WriteLine("Byte max value: " + byteMax);
+
+            short shortMin = short.MinValue;
+            short shortMax = short.MaxValue;
+
+            Console.WriteLine("Short min: " + shortMin);
+            Console.WriteLine("Short max: " + shortMax);
+
+            int intMin = int.MinValue;
+            int intMax = int.MaxValue;
+
+            Console.WriteLine("Int min: " +  intMin);
+            Console.WriteLine("Int max: " +  intMax);
+
+            long longMin = long.MinValue;
+            long longMax = long.MaxValue;
+
+            Console.WriteLine("Long min" +  longMin);
+            Console.WriteLine("Long max" +  longMax);
+
+            float floatMin = float.MinValue;
+            float floatMax = float.MaxValue;
+
+            Console.WriteLine("Float min" +  floatMin);
+            Console.WriteLine("Float max" +  floatMax);
+
+            double doubleMin = double.MinValue;
+            double doubleMax = double.MaxValue;
+
+            Console.WriteLine("Double min" +  doubleMin);
+            Console.WriteLine("Double max" +  doubleMax);
+
+
         }
     }
 }
